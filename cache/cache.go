25a5a19841cedab144700e5ca63ff5d09b44f432
MAJETIXo <maik332@mail.ru>
2025-05-20 20:29:56 +0300

    fear: solve tasks

--- cache/cache.go
+++ cache/cache.go
@@ -1,5 +1,7 @@
 package cache
 
+import "sync"
+
 // Interface - реализуйте этот интерфейс
 type Interface interface {
 	Set(k, v string)
@@ -9,21 +11,24 @@ type Interface interface {
 // Не меняйте названия структуры и название метода создания экземпляра Cache, иначе не будут проходить тесты
 
 type Cache struct {
-	// TODO: ваш код
+	mu    sync.RWMutex
+	store map[string]string
 }
 
 // NewCache создаёт и возвращает новый экземпляр Cache.
 func NewCache() Interface {
-	// TODO: ваш код
-	panic("implement me")
+	return &Cache{store: make(map[string]string)}
 }
 
-func (c Cache) Set(k, v string) {
-	// TODO implement me
-	panic("implement me")
+func (c *Cache) Set(k, v string) {
+	c.mu.Lock()
+	c.store[k] = v
+	c.mu.Unlock()
 }
 
-func (c Cache) Get(k string) (v string, ok bool) {
-	// TODO implement me
-	panic("implement me")
+func (c *Cache) Get(k string) (v string, ok bool) { // Я сделал указатели на кэш без них не получалось!!!
+	c.mu.RLock()
+	v, ok = c.store[k]
+	c.mu.RUnlock()
+	return
 }
