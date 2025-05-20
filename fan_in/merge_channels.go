a75e5e63c489fcc58db05e6b560e3a696296fe90
MAJETIXo <maik332@mail.ru>
2025-05-20 12:55:55 +0300

    fear: solve tasks

--- fan_in/merge_channels.go
+++ fan_in/merge_channels.go
@@ -1,9 +1,31 @@
 package fan_in
 
+import "sync"
+
 // MergeChannels - принимает несколько каналов на вход и объединяет их в один
 // Fan-in и merge channels синонимы
 func MergeChannels(channels ...<-chan int) <-chan int {
 	// TODO: ваш код
 
-	panic("implement me")
+	chout := make(chan int)
+
+	var wg sync.WaitGroup
+
+	output := func(ch <-chan int, id int) {
+		defer wg.Done()
+		for n := range ch {
+			chout <- n
+		}
+	}
+
+	wg.Add(len(channels))
+	for id, ch := range channels {
+		go output(ch, id)
+	}
+
+	go func() {
+		wg.Wait()
+		close(chout)
+	}()
+	return chout
 }
