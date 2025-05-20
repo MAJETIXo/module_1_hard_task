bdc71d58085ba44f8c475f791729bda27e6ec1fb
MAJETIXo <maik332@mail.ru>
2025-05-20 13:42:23 +0300

    fear: solve tasks

--- generate_password/generate_password.go
+++ generate_password/generate_password.go
@@ -1,10 +1,21 @@
 package generate_password
 
+import (
+	"crypto/rand"
+	"math/big"
+)
+
 // letters - список допустимых символов в пароле
 const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 func GeneratePassword(n int) (string, error) {
-	// TODO: ваш код
-
-	panic("implement me")
+	b := make([]byte, n)
+	for i := range b {
+		passw, err := rand.Int(rand.Reader, big.NewInt(int64(len(letters))))
+		if err != nil {
+			return "", err
+		}
+		b[i] = letters[passw.Int64()]
+	}
+	return string(b), nil
 }
